d2e66527febb289dafbaadac0ab0faff93a325a8
evgeniStas <evgeni_stas@swiego.com>
2018-02-01 12:27:02 +0200

    Change settings

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wpdb');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '09021994');
+define('DB_PASSWORD', 'dd429glk');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
