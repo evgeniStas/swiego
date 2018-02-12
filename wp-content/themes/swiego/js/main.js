e95acbf48c3b70d2231c5e3404067ff7d37597d8
evgeniStas <evgeni_stas@swiego.com>
2018-02-12 10:12:50 +0200

    Css settings

--- wp-content/themes/swiego/js/main.js
+++ wp-content/themes/swiego/js/main.js
@@ -512,5 +512,23 @@ function sendContact(ev) {
         }
     });
 }
+function loadCSS(href){
+    var ss = window.document.createElement('link'),
+        ref = window.document.getElementsByTagName('head')[0];
+
+    ss.rel = 'stylesheet';
+    ss.href = href;
+
+    // temporarily, set media to something non-matching to ensure it'll
+    // fetch without blocking render
+    ss.media = 'only x';
+
+    ref.parentNode.insertBefore(ss, ref);
+
+    setTimeout( function(){
+        // set media back to `all` so that the stylesheet applies once it loads
+        ss.media = 'all';
+    },0);
+}
 
 
