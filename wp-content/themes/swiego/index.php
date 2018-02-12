482714ff5738989a02f233ab11d046a4c8a58d72
evgeniStas <evgeni_stas@swiego.com>
2018-02-12 10:37:59 +0200

    js

--- wp-content/themes/swiego/index.php
+++ wp-content/themes/swiego/index.php
@@ -244,6 +244,7 @@ header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30
 
 <!-- jQuery -->
 
+<script async src="<?php echo get_template_directory_uri();?>/js/jquery.min.js"></script>
 <!-- jQuery Easing -->
 <script async src="<?php echo get_template_directory_uri();?>/js/jquery.easing.1.3.js"></script>
 <!-- Bootstrap -->
@@ -259,9 +260,9 @@ header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30
     <script async src='<?php echo get_template_directory_uri();?>/js/TweenLite.min.js'></script>
     <script async src='<?php echo get_template_directory_uri();?>/js/EasePack.min.js'></script>
 <script async src="<?php echo get_template_directory_uri();?>/js/jquery.flexslider-min.js"></script>
-<script src="<?php echo get_template_directory_uri();?>/js/canvas.js"></script>
+<script async src="<?php echo get_template_directory_uri();?>/js/canvas.js"></script>
 <!-- MAIN JS -->
-    <script src="<?php echo get_template_directory_uri();?>/js/main.js"></script>
+    <script async src="<?php echo get_template_directory_uri();?>/js/main.js"></script>
     <?php wp_footer() ?>
 
 </body>
